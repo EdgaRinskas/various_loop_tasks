e9ca136ed90801143c5e300d21757544674993e0
Edgaras Austinskas <eaustinskas@gmail.com>
2023-10-02 21:12:47 +0300

    1

---
+++ script.js
@@ -0,0 +1,90 @@
+//TASK1 multiplyBy2
+
+function multiplyBy2() {
+    for (let i = 1; i <= 100; i++) {
+        let result = i * 2;
+        console.log(i + " multiplied by 2 is " + result);
+    }
+}
+
+multiplyBy2();
+
+//TASK2 multiplyBy5
+
+function multiplyBy5() {
+    for (let i = 1; i <= 100; i++) {
+        let result = i * 5;
+        console.log(i + " multiplied by 5 is " + result);
+    }
+}
+
+multiplyBy5();
+
+
+//TASK3 add5ToNumber
+
+function add5ToNumber() {
+    for (let i = 1; i <= 100; i++) {
+        let result = i + 5;
+        console.log(i + " plus 5 is " + result);
+    }
+}
+
+add5ToNumber();
+
+
+//TASK4 subtract2FromNumber
+
+function subtract2FromNumber() {
+    for (let i = 1; i <= 100; i++) {
+        let result = i - 2;
+        console.log(i + " minus 2 is " + result);
+    }
+}
+
+subtract2FromNumber();
+
+
+//TASK5 squareNumber
+
+function squareNumber() {
+    for (let i = 1; i <= 100; i++) {
+        let result = i * i; // Square the number
+        console.log(i + " squared is " + result);
+    }
+}
+
+squareNumber();
+
+//TASK6 cubeNumber
+
+function cubeNumber() {
+    for (let i = 1; i <= 100; i++) {
+        let result = i ** 3; // Cube the number
+        console.log(i + " cubed is " + result);
+    }
+}
+
+cubeNumber();
+
+
+//TASK7
+
+for (let i = 100; i >= 1; i--) {
+    let result = i * 5; // Multiply each number by 5
+    console.log(i + " multiplied by 5 is " + result);
+}
+
+
+//TASK8
+
+function multiplyBy2() {
+    for (let i = 1; i <= 100; i++) {
+      let result = i * 2;
+      console.log(i + " * 2 = " + result);
+    }
+  }
+  
+  multiplyBy2();
+
+//TASK fizz buzz
